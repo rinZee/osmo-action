5235003d0b34bb08c6b8041abb6f25174523b3a3
unknown <trex33160@gmail.com>
2021-05-24 22:15:48 +0545

    added the images and video in public folder

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
-
+import "../styles/globals.css";
+import Head from "next/head";
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  <Head>
+    <title>Osmo Action</title>
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+    />
+  </Head>;
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
